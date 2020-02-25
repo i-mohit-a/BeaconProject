7073adecf047b0d1d26e6e9effa6f3a46df3daad
Mohit <agrawalmohit30@gmail.com>
2020-02-26 02:22:09 +0530

    Adding initial code

---
+++ app/src/main/java/com/example/dell/beaconproject/NotifyDist.java
@@ -0,0 +1,15 @@
+package com.example.dell.beaconproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotifyDist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify_dist);
+
+
+    }
+}
